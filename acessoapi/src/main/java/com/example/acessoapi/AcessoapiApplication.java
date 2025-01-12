fddd446cafbd43aff25a7e737c6c35fab79f40e6
Gabriel Rodrigues Pereira Rocha <gabrielrodrigues1927@gmail.com>
2025-01-11 21:06:26 -0300

    first commit

---
+++ acessoapi/src/main/java/com/example/acessoapi/AcessoapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.acessoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcessoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcessoapiApplication.class, args);
+	}
+
+}
---
+++ acessoapi/src/test/java/com/example/acessoapi/AcessoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.acessoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcessoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
